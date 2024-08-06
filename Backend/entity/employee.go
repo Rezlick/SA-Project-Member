b1ce204dc69255c7da6db7ea0dab9f5ba5199a3a
Peerawich Punyano <newpeerawich1579@gmail.com>
2024-08-06 22:48:26 +0700

    Update employee.go

--- Backend/entity/employee.go
+++ Backend/entity/employee.go
@@ -8,7 +8,7 @@ import(
 type Employee struct{
 	EmployeeID   	string 		`gorm:"primarykey" json:"employee_id"`
 	FirstName	string		`json:"first_name"`
-	Lastname	string		`json:"last_name"`
+	LastName	string		`json:"last_name"`
 	Username	string		`json:"username"`
 	Password	string		`json:"password"`
 	RegiterDate	time.Time	`json:"register_date"`
