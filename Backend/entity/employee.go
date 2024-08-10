4abddf8d41efb1d9b4970947a92619e5516181be
Peerawich Punyano <newpeerawich1579@gmail.com>
2024-08-10 22:05:58 +0700

    Update employee.go

--- Backend/entity/employee.go
+++ Backend/entity/employee.go
@@ -1,27 +1,22 @@
 package entity
 
 import(
-	"time"
 	"gorm.io/gorm"
 ) 
 
 type Employee struct{
-	EmployeeID   	string 		`gorm:"primarykey" json:"employee_id"`
-	FirstName	string		`json:"first_name"`
-	LastName	string		`json:"last_name"`
-	Username	string		`json:"username"`
-	Password	string		`json:"password"`
-	RegiterDate	time.Time	`json:"register_date"`
+	gorm.Model
+	FirstName		string		
+	LastName		string		
+	Username		string		
+	Password		string		
 
 	// FK from Position
-	PositionID	*uint
-	Position	Position 	`gorm:"foreignKey: position_id"`
+	PositionID		*uint
+	Position		Position 	`gorm:"foreignKey: position_id"`
 
 	// FK from Gender
-	GenderID	*uint
-	Gender		Gender 		`gorm:"foreignKey: gender_id"`
+	GenderID		*uint
+	Gender			Gender 		`gorm:"foreignKey: gender_id"`
 
-	CreatedAt 	time.Time
-	UpdatedAt 	time.Time
-	DeletedAt 	gorm.DeletedAt 	`gorm:"index"`
 }
