6634bf17c14aeb63ee02956fac3966326c6a546c
Peerawich Punyano <newpeerawich1579@gmail.com>
2024-08-28 00:42:39 +0700

    Create db.go

---
+++ Backend/config/db.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+   "fmt"
+   "github.com/Rezlick/SA_Project_Member/entity"
+   "gorm.io/driver/sqlite"
+   "gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func DB() *gorm.DB {
+   return db
+}
+
+func ConnectionDB() {
+   database, err := gorm.Open(sqlite.Open("Member.db?cache=shared"), &gorm.Config{})
+   if err != nil {
+       panic("failed to connect database")
+   }
+   fmt.Println("connected database")
+   db = database
+}
+
+func SetupDatabase() {
+	db.AutoMigrate(
+		&entity.Member{},
+		&entity.Employee{},
+		&entity.Gender{},
+		&entity.Rank{},
+		&entity.Position{},
+	)
+
+   GenderMale := entity.Gender{Name: "Male"}
+   GenderFemale := entity.Gender{Name: "Female"}
+
+   PositionAdmin := entity.Position{Name: "Admin"}
+   PositionManager := entity.Position{Name: "Manager"}
+
+   db.FirstOrCreate(&GenderMale, &entity.Gender{Name: "Male"})
+   db.FirstOrCreate(&GenderFemale, &entity.Gender{Name: "Female"})
+
+   db.FirstOrCreate(&PositionAdmin, &entity.Position{Name: "Admin"})
+   db.FirstOrCreate(&PositionManager, &entity.Position{Name: "Manger"})
+
+   hashedPassword, _ := HashPassword("12345")
+
+   employee := &entity.Employee{
+       FirstName: "Test",
+       LastName:  "Admin",
+       Email:  "admin@shabubuu.com",
+       Password:  hashedPassword,
+       GenderID: 1,
+	    PositionID: 1,
+   }
+
+   db.FirstOrCreate(employee, &entity.Employee{
+       Email: "admin@shabubuu.com",
+   })
+}
