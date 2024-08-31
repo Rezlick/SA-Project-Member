8f715acdc5b3a98064261772683de8b315ac51f4
Peerawich Punyano <newpeerawich1579@gmail.com>
2024-08-31 19:02:42 +0700

    Update db.go

--- Backend/config/db.go
+++ Backend/config/db.go
@@ -37,6 +37,14 @@ func SetupDatabase() {
    PositionAdmin := entity.Position{Name: "Admin"}
    PositionManager := entity.Position{Name: "Manager"}
 
+   RankBronze := entity.Rank{Name: "Bronze", Discount: 0.03}
+   RankSilver := entity.Rank{Name: "Silver", Discount: 0.05}
+   RankGold := entity.Rank{Name: "Gold", Discount: 0.07}
+
+   db.FirstOrCreate(&RankBronze, &entity.Rank{Name: "Bronze", Discount: 0.03})
+   db.FirstOrCreate(&RankSilver, &entity.Rank{Name: "Silver", Discount: 0.05})
+   db.FirstOrCreate(&RankGold, &entity.Rank{Name: "Gold", Discount: 0.07})
+
    db.FirstOrCreate(&GenderMale, &entity.Gender{Name: "Male"})
    db.FirstOrCreate(&GenderFemale, &entity.Gender{Name: "Female"})
 
