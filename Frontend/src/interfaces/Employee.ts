6602f2cfe007991a7149979d2264204f1f4298e0
Peerawich Punyano <newpeerawich1579@gmail.com>
2024-08-31 19:04:28 +0700

    Create Employee.ts

---
+++ Frontend/src/interfaces/Employee.ts
@@ -0,0 +1,9 @@
+export interface EmployeeInterface {
+    ID?: number;
+    FirstName?: string;
+    LastName?: string;
+    Email?: string;
+    Password?: string;
+    PositionID?: number;
+    GenderID?: number;
+}
