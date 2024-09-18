2441826e0ceb28628ab1a08e2e7dbafea4f9ff43
Peerawich Punyano <newpeerawich1579@gmail.com>
2024-09-18 20:51:00 +0700

    Update index.tsx

--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -1,15 +1,24 @@
 import { useRoutes, RouteObject } from "react-router-dom";
 import AdminRoutes from "./AdminRoutes";
-import MainRoutes from "./MainRoutes";
+import LoginRoutes from "./LoginRoutes";
 
 function ConfigRoutes() {
   const isLoggedIn = localStorage.getItem("isLogin") === "true";
+  const positionID = localStorage.getItem("positionID"); 
+  let role = "";
+  if (positionID === '1') {
+    role = "IT"
+  } else if (positionID === '2'){
+    role = "Manager"
+  } else {
+    role = "Common"
+  }
   let routes: RouteObject[] = [];
 
   if (isLoggedIn) {
-    routes = [AdminRoutes(isLoggedIn), MainRoutes()];
+    routes = [AdminRoutes(isLoggedIn, role || "Common"), LoginRoutes()];
   } else {
-    routes = [MainRoutes()];
+    routes = [LoginRoutes()];
   }
 
   return useRoutes(routes);
